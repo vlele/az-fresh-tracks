ab1d79a019a93e4ce27dabab918c07b302149a59
Praneeth Gadhamsetty <gvpraneeth@gmail.com>
2021-10-27 12:23:38 +0530

    initial checkin

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+/* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0 */
+
+<template>
+  <div id="app" class="d-flex flex-column h-100">
+    <nav-bar />
+    <div class="container flex-grow-1">
+      <error />
+      <div class="mt-5">
+        <router-view />
+      </div>
+    </div>
+    <footer class="bg-light text-center p-3">
+        <a href="https://github.com/aws-samples/fresh-Tracks">
+      <div class="logo"></div>
+      <p>
+        Source code available on Github
+      </p>
+      </a>
+        <p class="site-footer__copy">
+              <a href="https://aws.amazon.com/privacy/">Privacy</a> | 
+              <a href="https://aws.amazon.com/terms/">Site Terms</a> |
+              &copy; Copyright 2020 Amazon.com, Inc. or its affiliates. All rights reserved.
+        </p><!-- /.site-footer__copy -->
+
+    </footer>
+  </div>
+</template>
+
+<script>
+
+
+
+import NavBar from "./components/NavBar";
+import Error from "./components/Error";
+
+
+export default {
+  
+  components: {
+    NavBar,
+    Error
+  }
+  
+};
+</script>
---
+++ src/components/HomeContent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="next-steps">
+
+
+     <p class="lead">
+    <br>
+    <br>
+    This app demonstrates how to extend the capability of your custom built applicaitons by integrating Amazon Event Bridge's 3P SaaS partners.  Read aboout the example integrations below:
+    <br><br>
+    </p>
+
+    <div class="row">
+      
+    </div> 
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "HomeContent"
+};
+</script>
