571f09e4828e133072aa2eda0d9b25cc6174e1c6
Praneeth Gadhamsetty <gvpraneeth@gmail.com>
2021-10-31 13:07:31 +0530

    Orchestrator Flow Added

---
+++ Backend/Functions/FileCreatedTrigger/index.js
@@ -0,0 +1,21 @@
+const axios = require('axios');
+
+module.exports = async function (context, eventGridEvent) {
+    context.log(typeof eventGridEvent);
+    context.log(eventGridEvent);
+    context.log('My Trigger is called...');
+
+    const url = 'https://durablefunctionexample.azurewebsites.net/api/orchestrators/ProcessFileOrchestrator';
+
+    axios
+        .post(url, eventGridEvent)
+        .then(res => {
+            context.log('My Trigger API Response...');
+            context.log(res);
+            console.log(`statusCode: ${res.status}`)
+            console.log(res)
+        })
+        .catch(error => {
+            console.error(error)
+        })
+};
